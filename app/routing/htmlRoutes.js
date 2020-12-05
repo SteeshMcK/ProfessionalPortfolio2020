adff8e2f1d4fd8bb55c32405a6f897fbbcf0b82b
SteeshMcK <stesha.norstrom@pcc.edu>
2020-12-04 20:21:53 -0800

    re-structure files

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,10 @@
 const path = require('path');
+const express = require('express')
+const app = express()
 
 module.exports = function(app) {
+
+    app.use(express.static(path.join(__dirname, 'public/dist')));
 
     app.use(function(req, res) {
         res.sendFile(path.join(__dirname + '/../public/index.html'))
