467db68042c802125b778a0450297653b7ff42bc
SteeshMcK <stesha.norstrom@pcc.edu>
2020-12-04 22:13:43 -0800

    try to connect css to html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,9 +4,7 @@ const app = express()
 
 module.exports = function(app) {
 
-    app.use(express.static(path.join(__dirname, 'public/dist')));
-
     app.use(function(req, res) {
-        res.sendFile(path.join(__dirname + '/../public/index.html'))
+        res.sendFile(path.join(__dirname + '/../views/index.html'))
     })
 }
--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.urlencoded({extended: true})); 
-app.use(express.static(path.join(__dirname, 'public/dist')));
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname + 'public')))
 
 
 require("./app/routing/htmlRoutes.js")(app);
